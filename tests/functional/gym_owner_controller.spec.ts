e940a215f252d21505a78f7d67e9e179197bf1c8
Mathis SPORTIELLO <m-sportiello@it-systemes.fr>
2025-07-23 09:02:23 +0200

    Suppression de plusieurs fichiers de tests, y compris des tests fonctionnels et unitaires pour les contrôleurs, entités et cas d'utilisation, afin de nettoyer le codebase.

--- tests/functional/gym_owner_controller.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@japa/runner'
-
-test.group('GymOwnerController', () => {
-  test('should be defined', async ({ assert }) => {
-    // Simplified test - actual controller methods need implementation
-    assert.isTrue(true)
-  })
-})
